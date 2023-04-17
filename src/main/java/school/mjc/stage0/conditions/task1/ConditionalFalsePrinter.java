8234d3eea5a606787f4557c37267cb6289ea9bb7
Akzhol <cheder911@gmail.com>
2023-04-17 19:23:08 +0600

    1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a = 1>2;
+        if (a==true) {
+            System.out.println("true");
+        }
+            else {
+                System.out.println("false");
+            }
     }
 }
+
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean a = 1>0;
+        if (a==true){
+            System.out.println("true");
+        }
     }
 }
